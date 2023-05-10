2e14b0613d05e311676d7b2da85567a1276522c4
unknown <alejandroisa71@gamail.com>
2023-05-10 20:03:05 -0300

    Se estiliza pagina principal y TaskCard

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,6 +1,6 @@
-import { useRouter } from 'next/navigation';
-import { useTasks } from '../context/TaskContext';
-import {toast} from 'react-hot-toast'
+import { useRouter } from "next/navigation";
+import { useTasks } from "../context/TaskContext";
+import { toast } from "react-hot-toast";
 
 const TaskCard = ({ task }) => {
   const router = useRouter();
@@ -8,22 +8,27 @@ const TaskCard = ({ task }) => {
   return (
     <div
       key={task.id}
-      style={{ background: '#202020', color: 'white' }}
+      className="bg-gray-700 hover:bg-gray-600 cursor-pointer px-20 py-5 m-2 "
       onClick={() => router.push(`/edit/${task.id}`)}
     >
-      <h1>{task.title}</h1>
-      <button
-        onClick={(e) => {
-          e.stopPropagation();
-          const accept = window.confirm('are you sure');
-          if (accept) {deleteTask(task.id);
-            toast.success('task deleted successfully')
-          }
-        }}
-      >
-        Delete
-      </button>
-      <p>{task.description}</p>
+      <div className="flex justify-between">
+        <h1>{task.title}</h1>
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            const accept = window.confirm("are you sure");
+            if (accept) {
+              deleteTask(task.id);
+              toast.success("task deleted successfully");
+            }
+          }}
+          className="bg-red-700 hover:bg-red-600 rounded px-3 py-1 inline-flex items-center"
+        >
+          Delete
+        </button>
+      </div>
+      <p className="text-gray-300">{task.description}</p>
+      <span className="text-gray-400 text-xs">{task.id}</span>
     </div>
   );
 };

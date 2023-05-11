8428bacce87067a946228ca799d70e6872337375
Alejandro <alejandroisa71@gmail.com>
2023-05-10 22:15:00 -0300

    Se estiliza el formulario y al Navbar se le agrega sapan con cantidad de tareas

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useTasks } from '../../context/TaskContext';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
-import {toast} from 'react-hot-toast'
+import { toast } from 'react-hot-toast';
 
 const TaskFormPage = ({ params }) => {
   const {
@@ -39,22 +39,28 @@ const TaskFormPage = ({ params }) => {
   }, []);
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        placeholder="Write a title"
-        autoFocus
-        name='title'
-        {...register('title', { required: true })}
-      />
-      {errors.title && <span>This field is required</span>}
-      <textarea
-        placeholder="Write a description"
-        {...register('description', { required: true })}
-        name='description'
-      />
-      {errors.description && <span>This field is required</span>}
-      <button>Save</button>
-    </form>
+    <div className="flex justify-center items-centerh-full">
+      <form onSubmit={onSubmit} className="bg-gray-700 p-10">
+        <h2>New Task</h2>
+        <input
+          className="bg-gray-800 py-3 px-4 mb-2 block focus:outline-none w-full"
+          placeholder="Write a title"
+          autoFocus
+          name="title"
+          {...register('title', { required: true })}
+        />
+        {errors.title && <span  className='block text-red-400 mb-2'>This field is required</span>}
+        <textarea
+          className="bg-gray-800 py-3 px-4 mb-2 block focus:outline-none w-full"
+          placeholder="Write a description"
+          {...register('description', { required: true })}
+          name="description"
+        />
+        {errors.description && <span  className='block text-red-400 mb-2'>This field is required</span>}
+        <button className='bg-green-500 hover:bg-green-400 py-2 px-4
+         rounded-sm disabled:opacity-30'>Save</button>
+      </form>
+    </div>
   );
 };
 export default TaskFormPage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 "use client";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useTasks } from "@/context/TaskContext";
 const Navbar = () => {
   const router = useRouter();
+  const{tasks}=useTasks();
   return (
     <header className="flex  justify-between items-center bg-gray-800 px-28 py-3">
       <Link href="/">
-        <h1 className="font-bold text-3xl text-white">Task App</h1>
+        <h1 className="font-bold text-3xl text-white">Task App <span className="text-slate-300 text-sm ml-5">{tasks.length} tasks</span></h1>
       </Link>
       <div>
         <button
